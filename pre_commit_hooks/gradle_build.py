9e62e82cacc6c1582dc50343021f5be64cd6b52d
Dzhem Aptula - TomTom <dzhem.aptula@tomtom.com>
2022-02-21 13:56:57 +0100

    Add support to exclude tasks

--- pre_commit_hooks/gradle_build.py
+++ pre_commit_hooks/gradle_build.py
@@ -17,12 +17,19 @@ def main(argv=None):  # type: (Optional[Sequence[str]]) -> int
         '-o', '--output', action='store_true',
         help='Prints the output of all executed gradle commands.'
     )
+    parser.add_argument(
+        '--exclude-task', action='store', dest='exclude_task', type=str,
+        help='Exclude given task during the command.'
+    )
     args = parser.parse_args(argv)
 
+    skip_test_arg = f' -x {args.exclude}' if args.exclude else '';
+    task = 'build' + skip_test_arg
+
     if args.wrapper:
-        return run_gradle_wrapper_task(args.output, 'build')
+        return run_gradle_wrapper_task(args.output, task)
     else:
-        return run_gradle_task(args.output, 'build')
+        return run_gradle_task(args.output, task)
 
 
 if __name__ == '__main__':
